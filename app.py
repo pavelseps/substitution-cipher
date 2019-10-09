03fe1790298750647aa9ba9f80d5e9d16f98fb52
Pavel Šeps <pavel.seps@websta.de>
2019-10-09 22:01:56 +0200

    try to decode cipher

--- app.py
+++ app.py
@@ -3,20 +3,19 @@ from lib.Dictionary import Dictionary
 
 text_to_decode = "YWUY XWDDMTKBT, OR YWUYKBT, KD YLW MPY OE POXQODKBT MBC DWBCKBT WVWPYROBKP XWDDMTWD, YNQKPMVVN POBDKDYKBT OE MVQLMSWYKP MBC BGXWRKP PLMRMPYWRD, SWYHWWB YHO OR XORW GDWRD OE XOSKVW CWIKPWD, CWDAYOQD/VMQYOQD, OR OYLWR YNQW OE POXQMYKSVW POXQGYWR. YWUY XWDDMTWD XMN SW DWBY OIWR M PWVVGVMR BWYHORA, OR XMN MVDO SW DWBY IKM MB KBYWRBWY POBBWPYKOB.YLW YWRX ORKTKBMVVN RWEWRRWC YO XWDDMTWD DWBY GDKBT YLW DLORY XWDDMTW DWRIKPW (DXD). KY LMD TROHB SWNOBC MVQLMBGXWRKP YWUY YO KBPVGCW XGVYKXWCKM XWDDMTWD (ABOHB MD XXD) POBYMKBKBT CKTKYMV KXMTWD, IKCWOD, MBC DOGBC POBYWBY, MD HWVV MD KCWOTRMXD ABOHB MD WXOZK (LMQQN EMPWD, DMC EMPWD, MBC OYLWR KPOBD)."
 
-
+# load Dictionary
 dict = Dictionary("./dictionary.txt")
 dict.load()
 dict.process_dict_pattern()
 
-# pattern = dict.word_to_pattern("ABED")
-pattern = [0, 1, 1]
-print(pattern)
-print(dict.find_pattern(pattern))
-
-exit()
-
+# Load Cipher
 cip = Cipher()
 cip.set_dictionary(dict)
-cip.set_original_text("Hi I am car.")
-cip.encode()
-print(cip.get_cipher_text())
+# cip.set_original_text("Hi I am car.")
+# cip.encode()
+# print(cip.get_cipher_text())
+# cip.set_cipher_text("YWUY XWDDMTKBT, OR YWUYKBT, KD YLW MPY OE POXQODKBT MBC DWBCKBT WVWPYROBKP XWDDMTWD")
+cip.set_cipher_text(text_to_decode)
+cip.decode()
+print("Cipher text: ", cip.get_cipher_text())
+print("Decoded text: ", cip.get_original_text())
--- lib/Cipher.py
+++ lib/Cipher.py
@@ -1,8 +1,9 @@
 import numpy as np
 import random
+import operator
 
 ALPHABET = np.array(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"])
-SKIP = np.array([" ", ".", ",", "(", ")", "[", "]"])
+SKIP = np.array([" ", ".", ",", ":",  ";", "(", ")", "[", "]", "?", "!", "\"", "'", "\\", "/"])
 
 
 class Cipher:
@@ -27,13 +28,10 @@ class Cipher:
         return self.cipher
 
     def encode(self):
+        print("Cipher encode: Start")
         # Set data to work
         alph = ALPHABET[:]
-        alph_dict = dict()
-
-        for i in range(len(alph)):
-            alph_dict[alph[i]] = None
-
+        alph_dict = self._get_alphabet_dict()
 
         self.cipher = ""
 
@@ -61,7 +59,68 @@ class Cipher:
             else:
                 self.cipher += char
 
+        print("Cipher encode: Done")
 
     def decode(self):
-        pass
+        print("Cipher decode: Start")
+        # Set data to work
+        appearance_dict = self._get_alphabet_dict_funct(lambda : self._get_alphabet_dict(0))
+        cipher = (self.cipher + '.')[:-1]
 
+        # Remove unwanted chars except space
+        for i in range(len(SKIP)):
+            if SKIP[i] != " ":
+                cipher = cipher.replace(SKIP[i], "")
+
+        cipher_array = cipher.split()
+
+        # Process each word
+        for i in range(len(cipher_array)):
+            word = cipher_array[i]
+            try:
+                pattern = self.dictionary.word_to_pattern(word)
+                candidates = self.dictionary.find_pattern(pattern)
+
+                # Set number of appearance
+                for word_char_i in range(len(word)):
+                    for candidate_word_i in range(len(candidates)):
+                        candidate_word = candidates[candidate_word_i]
+                        appearance_dict[word[word_char_i]][candidate_word[word_char_i]] += 1
+            except:
+                print("Can not find in dictionary:", word)
+
+        translate_dict = self._get_alphabet_dict()
+
+        # Create char to char dictionary (key cipher, val original)
+        for char in translate_dict:
+            max_item = max(appearance_dict[char].items(), key=operator.itemgetter(1))
+
+            if max_item[1] > 0:
+                translate_dict[char] = max_item[0]
+                total = sum(appearance_dict[char].values())
+                print(char + " is " + translate_dict[char] + ": " + str(((max_item[1] / total) * 100)) + "%")
+
+        # Write original
+        self.original = ""
+        for char_c in self.cipher:
+            if not np.isin(char_c, SKIP) and translate_dict[char_c] is not None:
+                self.original += translate_dict[char_c]
+            else:
+                self.original += char_c
+
+        print("Cipher encode: Done")
+
+
+    def _get_alphabet_dict(self, default = None):
+        alph_dict = dict()
+
+        for i in range(len(ALPHABET)):
+            alph_dict[ALPHABET[i]] = default
+        return alph_dict
+
+    def _get_alphabet_dict_funct(self, funct):
+        alph_dict = dict()
+
+        for i in range(len(ALPHABET)):
+            alph_dict[ALPHABET[i]] = funct()
+        return alph_dict
