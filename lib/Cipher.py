7bac8e995c239725a10e7431f5136efaf885d6c7
Pavel Šeps <pavel.seps@websta.de>
2019-10-10 21:55:20 +0200

    decode cipher v2

--- lib/Cipher.py
+++ lib/Cipher.py
@@ -64,7 +64,8 @@ class Cipher:
     def decode(self):
         print("Cipher decode: Start")
         # Set data to work
-        appearance_dict = self._get_alphabet_dict_funct(lambda : self._get_alphabet_dict(0))
+        appearance_dict = self._get_alphabet_dict(np.array([]))
+        intersect_dict = self._get_alphabet_dict(np.array([]))
         cipher = (self.cipher + '.')[:-1]
 
         # Remove unwanted chars except space
@@ -85,26 +86,26 @@ class Cipher:
                 for word_char_i in range(len(word)):
                     for candidate_word_i in range(len(candidates)):
                         candidate_word = candidates[candidate_word_i]
-                        appearance_dict[word[word_char_i]][candidate_word[word_char_i]] += 1
+                        appearance_dict[word[word_char_i]] = np.append(appearance_dict[word[word_char_i]],
+                                                                       candidate_word[word_char_i])
+
+                # Intersect candidates
+                intersect_dict = self.intersect_dicts(intersect_dict, appearance_dict)
+                appearance_dict = self._get_alphabet_dict(np.array([]))
+
             except:
                 print("Can not find in dictionary:", word)
 
-        translate_dict = self._get_alphabet_dict()
+        intersect_dict = self.remove_solved_letters(intersect_dict)
 
-        # Create char to char dictionary (key cipher, val original)
-        for char in translate_dict:
-            max_item = max(appearance_dict[char].items(), key=operator.itemgetter(1))
-
-            if max_item[1] > 0:
-                translate_dict[char] = max_item[0]
-                total = sum(appearance_dict[char].values())
-                print(char + " is " + translate_dict[char] + ": " + str(((max_item[1] / total) * 100)) + "%")
+        # Print dict stats
+        self.print_stats(intersect_dict)
 
         # Write original
         self.original = ""
         for char_c in self.cipher:
-            if not np.isin(char_c, SKIP) and translate_dict[char_c] is not None:
-                self.original += translate_dict[char_c]
+            if not np.isin(char_c, SKIP) and len(intersect_dict[char_c]) > 0:
+                self.original += intersect_dict[char_c][0]
             else:
                 self.original += char_c
 
@@ -124,3 +125,60 @@ class Cipher:
         for i in range(len(ALPHABET)):
             alph_dict[ALPHABET[i]] = funct()
         return alph_dict
+
+    def intersect_dicts(self, intersect_dict, appearance_dict):
+        result = self._get_alphabet_dict(np.array([]))
+
+        for char in result:
+            if len(appearance_dict[char]) > 0:
+                if len(intersect_dict[char]) < 1:
+                    result[char] = np.unique(appearance_dict[char])
+                else:
+                    result[char] = np.intersect1d(intersect_dict[char], np.unique(appearance_dict[char]))
+            else:
+                result[char] = np.unique(intersect_dict[char])
+
+        return result
+
+    def print_stats(self, intersect_dict):
+        missing = np.array([])
+        unclear = dict()
+        correct = dict()
+
+        for char in intersect_dict:
+            if len(intersect_dict[char]) < 1:
+                missing = np.append(missing, char)
+            elif len(intersect_dict[char]) == 1:
+                correct[char] = intersect_dict[char][0]
+            elif len(intersect_dict[char]) > 1:
+                unclear[char] = intersect_dict[char]
+        print("CORRECT: "+ str(int((len(correct) / len(intersect_dict)) * 100)) +"%", correct)
+        print("UNCLEAR: "+ str(int((len(unclear) / len(intersect_dict)) * 100)) +"%", unclear)
+        print("MISSING: "+ str(int((len(missing) / len(intersect_dict)) * 100)) +"%", missing)
+
+    def remove_solved_letters(self, intersect_dict):
+        # then removeSolvedLettersFromMapping and try guess last char
+        alph = ALPHABET[:]
+
+        # remove all ready found
+        to_remove_index = np.array([])
+        for i in range(len(alph)):
+            char = alph[i]
+            if(len(intersect_dict[char]) == 1):
+                to_remove_index = np.append(to_remove_index, np.where(alph == intersect_dict[char][0]))
+
+        alph = np.delete(alph, np.unique(to_remove_index))
+
+        # check dict and remove allready found chars
+        is_changed = False
+        for char in intersect_dict:
+            if(len(intersect_dict[char]) > 1):
+                intersect_dict[char] = np.intersect1d(alph, intersect_dict[char])
+
+                if len(intersect_dict[char]) == 1:
+                    is_changed = True
+
+        if is_changed:
+            return self.remove_solved_letters(intersect_dict)
+        else:
+            return intersect_dict
